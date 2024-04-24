6533a561e7bffda1f022bb6515b9f6cb8d2b9b25
AJ Mauney <amauney@captechventures.com>
2024-04-24 13:16:26 -0400

    made lambda function with api gateway;

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var person Person
+	err := json.Unmarshal([]byte(request.Body), &person)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: `{"error": "Internal server error encountered when unmarshalling}`,
+		}, err
+	}
+
+	msg := fmt.Sprintf("Hello %v, your id is: %v and your message is: %v", *person.FirstName, *person.Id, *person.Msg)
+	responseBody := ResponseBody{
+		Message: &msg,
+	}
+
+	jbytes, err := json.Marshal(responseBody)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: `{"error": "Internal server error encountered when marshalling}`,
+		}, err
+	}
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(jbytes),
+		Headers: map[string]string{
+			"Content-Type":                     "text/plain",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Headers":     "Content-Type",
+			"Access-Control-Allow-Methods":     "OPTIONS, POST, GET",
+			"Access-Control-Allow-Credentials": "true",
+		},
+	}
+
+	return response, nil
+
+}
+
+type Person struct {
+	Id        *string `json:"id"`
+	Msg       *string `json:"message"`
+	FirstName *string `json:"firstName"`
+}
+
+type ResponseBody struct {
+	Message *string `json:"message"`
+}
